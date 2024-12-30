89e3b1a69b31e59ab4c755a105aa041d6e8c84d0
BetoCruz <humbertocruz.sp@gmail.com>
2024-12-29 23:34:32 -0300

    adicionando seção footer

--- src/main.js
+++ src/main.js
@@ -1,18 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    // const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions =document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(botao){
+            buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
+            console.log(aba);
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__list--is-active');
-        });        
+        })        
+    }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento.target.parentNode);
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe);
+    
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
