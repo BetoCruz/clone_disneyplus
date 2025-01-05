98be00bac385f7f6011c32b813d887f00ff5a033
BetoCruz <humbertocruz.sp@gmail.com>
2025-01-05 17:18:10 -0300

    Configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 // function testeGulp(cb){
 //     console.log("Olá mundo");
 //     cb();
 // }
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -20,8 +27,9 @@ function images(){
 }
 
 // exports.default = testeGulp;
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*js', gulp.parallel(scripts));
 }
